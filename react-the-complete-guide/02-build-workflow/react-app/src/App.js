a0ac3fb633f4eb62387dc4c615fdcaed85504637
dhantosy <dhantosy@gmail.com>
2018-12-13 15:36:39 +0700

    added person component and create props to communicate between component

--- react-the-complete-guide/02-build-workflow/react-app/src/App.js
+++ react-the-complete-guide/02-build-workflow/react-app/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
+    // return React.createElement('div', { className: 'App' }, 
+    //   React.createElement('h1', {}, 'Hello!'));
+    // is the same as:
     return (
       <div className="App">
-        <h1>Hi</h1>
+        <h1>Hi!</h1>
+        { /* pass props to children */ }
+        <Person name="Dhanto" age="30" />
+        { /* anything between opening and closing of component, is includes in props.children */ }
+        <Person name="Santika" age="28">So fun!</Person>
+        <button>Switch Name</button>
       </div>
     );
   }
---
+++ react-the-complete-guide/02-build-workflow/react-app/src/Person/Person.js
@@ -0,0 +1,20 @@
+// when creating components, you have the choice between two different ways:
+// 1. Functional components (also referred to as "presentational", "dumb" or "stateless" components - more about this later in the course) => const cmp = () => { return <div>some JSX</div> } (using ES6 arrow functions as shown here is recommended but optional)
+// 2. class-based components (also referred to as "containers", "smart" or "stateful" components) => class Cmp extends Component { render () { return <div>some JSX</div> } } 
+
+// do not have to import component because it contains only a function.
+import React from 'react';
+
+// passing props from parent component
+const person = (props) => {
+  // use parenthesis to enable multiline
+  return ( 
+    <div>
+      <p>I'm { props.name } and I am { props.age } years old!</p>
+      { /* children refers to elements between opening and closing in the component */ }
+      <p>{ props.children }</p>
+    </div>
+    )
+}
+
+export default person;
