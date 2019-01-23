fbcc5571c04a5aecdbf0813ee5717f7dbe520304
dhantosy <dhantosy@gmail.com>
2019-01-23 17:45:12 +0700

    convert stateless to stateful

--- react-the-complete-guide/02-build-workflow/react-app/src/Persons/Person/Person.js
+++ react-the-complete-guide/02-build-workflow/react-app/src/Persons/Person/Person.js
@@ -3,23 +3,36 @@
 // 2. class-based components (also referred to as "containers", "smart" or "stateful" components) => class Cmp extends Component { render () { return <div>some JSX</div> } } 
 
 // do not have to import component because it contains only a function.
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 // import Radium from 'radium'; // enables pseudo and media query in inline styles
 
 // passing props from parent component
-const person = (props) => {
+// const person = (props) => {
 
-  // use parenthesis to enable multiline
-  return ( 
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-      { /* children refers to elements between opening and closing in the component */ }
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+//   // use parenthesis to enable multiline
+//   return ( 
+//     <div className={classes.Person}>
+//       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+//       <p>{props.children}</p>
+//       <input type="text" onChange={props.changed} value={props.name} />
+//     </div>
+//     )
+// }
+
+// // export default Radium(person);
+// export default person;
+
+class Person extends Component {
+  render () {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
     )
+  }
 }
 
-// export default Radium(person);
-export default person;
+export default Person;
