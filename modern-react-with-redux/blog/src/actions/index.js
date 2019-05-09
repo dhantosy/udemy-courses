1f5494985c1c0699a1bb45d7f4b4a24ead81434e
dhantosy <dhantosy@gmail.com>
2019-05-09 10:34:58 +0700

    alternate way to solve duplicated call request

--- modern-react-with-redux/blog/src/actions/index.js
+++ modern-react-with-redux/blog/src/actions/index.js
@@ -7,19 +7,36 @@ export const fetchPosts = () =>  async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
 
-export const fetchUser = (id) => dispatch => {
-  _fetchUser(id, dispatch);
-}
+// using memoize
+// export const fetchUser = (id) => dispatch => {
+//   _fetchUser(id, dispatch);
+// }
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: 'FETCH_USER', payload: response.data })
+// });
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
-  dispatch({ type: 'FETCH_USER', payload: response.data })
-});
-
+// default version
 // export const fetchUser = function(id) {
 //   return async function (dispatch) {
 //     const response = await jsonPlaceholder.get(`/users/${id}`);
 //     dispatch({ type: 'FETCH_USER', payload: response.data })
 //   }
 // }
+
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data })
+};
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userId = _.uniq(_.map(getState().posts, 'userId'));
+
+  userId.forEach(id => dispatch(fetchUser(id)));
+
+};
+
+
+
 
