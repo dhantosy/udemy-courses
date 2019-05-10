0593c91ff39c5c2b1170da208b1962dd4dc455b5
dhantosy <dhantosy@gmail.com>
2019-05-10 14:12:59 +0700

    included redux form, and added input text

--- modern-react-with-redux/streams/client/src/components/streams/StreamCreate.js
+++ modern-react-with-redux/streams/client/src/components/streams/StreamCreate.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return (
-    <div>StreamCreate</div>
-  )
-};
+class StreamCreate extends React.Component {
 
-export default StreamCreate;
+  renderInput({ input, label }) {
+    return (
+        <div className='field'>
+          <label>{label}</label>
+          <input type='text' {...input} />
+        </div>
+    )
+  }
+
+  render() {
+    return (
+      <form className='ui form'>
+        <Field name='stream_title' component={this.renderInput} label='Enter Title' />
+        <Field name='stream_description' component={this.renderInput} label='Enter Description' />
+      </form>
+    );
+  }
+
+}
+
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
