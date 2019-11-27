90b1b72af21c1890b637a16796d5f3bcf9a3e5bf
dhantosy <dhantosy@gmail.com>
2019-11-27 20:53:29 +0700

    added function to check if user exist

--- mern-stack-front-back/devconnector/routes/api/users.js
+++ mern-stack-front-back/devconnector/routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/User');
+
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
@@ -13,13 +15,38 @@ router.post('/', [
     .isEmail(),
   check('password', 'Please enter a password with 6 or more characters')
     .isLength({ min: 6 })
-], (req, res) => {
+], 
+async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  res.send('User route')
+  const { name, email, password } = req.body;
+
+  try {
+
+    // Check if user exist
+    let user = await User.findOne({ email });
+
+    if (user) {
+      res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+    }
+
+
+    // Get users gravatar
+
+    // Encrypt password
+
+    // return JWT
+
+    res.send('User route')
+
+  } catch(err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+
 });
 
 module.exports = router;
