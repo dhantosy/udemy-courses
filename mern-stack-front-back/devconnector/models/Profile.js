04c78f9aa1f0f1115cd8ebddb468337174d888f1
dhantosy <dhantosy@gmail.com>
2019-11-28 10:52:01 +0700

    created get profile route

--- mern-stack-front-back/devconnector/models/Profile.js
+++ mern-stack-front-back/devconnector/models/Profile.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user'
