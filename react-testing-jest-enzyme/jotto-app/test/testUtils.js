539e982d32d339378828b2dfb28a30ad7c41a400
dhantosy <dhantosy@gmail.com>
2020-01-29 16:28:01 +0700

    setup input component and storeFactory test util

--- react-testing-jest-enzyme/jotto-app/test/testUtils.js
+++ react-testing-jest-enzyme/jotto-app/test/testUtils.js
@@ -1,6 +1,20 @@
 import CheckPropTypes from 'check-prop-types';
 import propTypes from 'prop-types';
 
+import rootReducer from '../src/reducers';
+import { createStore } from 'redux';
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state.
+ * globasl: rootReducer
+ * @param {object} initialState
+ * @function storeFactory
+ * @returns {Store} - redux store
+ */
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+}
+
 /**
  * Return node(s) with the given data-test attr.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper
