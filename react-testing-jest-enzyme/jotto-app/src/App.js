1bb4f87a212013229d86794e6fb213e0edfa540a
dhantosy <dhantosy@gmail.com>
2020-01-30 16:53:02 +0700

    connect app component and tests

--- react-testing-jest-enzyme/jotto-app/src/App.js
+++ react-testing-jest-enzyme/jotto-app/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from './Input';
+import { getSecretWord } from './actions';
 
 class App extends Component {
 
@@ -10,13 +13,16 @@ class App extends Component {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[
-          { guessedWord: 'train', letterMatchCount: 3 }
-        ]} />
+        <Congrats success={this.props.success} />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+}
+
+export default connect(mapStateToProps, { getSecretWord })(App);
