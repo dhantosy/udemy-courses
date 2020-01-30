b333c5c2df4de802d22d6f3fd264e52641008728
dhantosy <dhantosy@gmail.com>
2020-01-30 09:49:53 +0700

    install redux thunk

--- react-testing-jest-enzyme/jotto-app/test/testUtils.js
+++ react-testing-jest-enzyme/jotto-app/test/testUtils.js
@@ -1,8 +1,8 @@
 import CheckPropTypes from 'check-prop-types';
-import propTypes from 'prop-types';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
-import { createStore } from 'redux';
+import { middlewares } from '../src/configureStore';
 
 /**
  * Create a testing store with imported reducers, middleware, and initial state.
@@ -12,7 +12,8 @@ import { createStore } from 'redux';
  * @returns {Store} - redux store
  */
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
 
 /**
