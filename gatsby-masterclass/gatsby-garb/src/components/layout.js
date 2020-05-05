a2f445bf92208445ab8f18399740a40824717ee0
dhantosy <dhantosy@gmail.com>
2020-05-05 15:51:08 +0700

    change useStaticQuery to StaticQuery, and update the code

--- gatsby-masterclass/gatsby-garb/src/components/layout.js
+++ gatsby-masterclass/gatsby-garb/src/components/layout.js
@@ -7,40 +7,46 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
+const GET_SITE_METADATA = graphql`
+{
+  site {
+    siteMetadata {
+      title
+      author
     }
-  `)
+  }
+}
+`
+
+const Layout = ({ children }) => {
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <StaticQuery
+      query={GET_SITE_METADATA}
+      render={data => (
+        <>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0 1.0875rem 1.45rem`,
+            }}
+          >
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built by {data.site.siteMetadata.author}
+            </footer>
+          </div>
+        </>
+      )}
+    
+    />
   )
 }
 
