b87d0ac67fe14afb2846d7d2384729c28dd6e364
dhantosy <dhantosy@gmail.com>
2020-05-05 09:56:55 +0700

    import layout and use link

--- gatsby-masterclass/gatsby-garb/src/pages/page-3.js
+++ gatsby-masterclass/gatsby-garb/src/pages/page-3.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Layout from '../components/layout';
+import { Link } from 'gatsby';
 
 export default () => (
-  <div>
+  <Layout>
     <h1>hellow1</h1>
-  </div>
+    <Link to='/'>go to home</Link>
+  </Layout>
 )
