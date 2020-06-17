08e4d30844d9915908e0f2a908d3d2a60ce84a47
dhantosy <dhantosy@gmail.com>
2020-06-17 21:34:24 +0700

    added app & stack test cases, and added fixture data file

--- react-redux-enzyme/flashcardpro/src/components/App.js
+++ react-redux-enzyme/flashcardpro/src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h2>Flascard Pro</h2>
+        <h2>Flashcard Pro</h2>
         <hr />
         <StackList />
         <hr />
--- react-redux-enzyme/flashcardpro/src/components/App.test.js
+++ react-redux-enzyme/flashcardpro/src/components/App.test.js
@@ -5,7 +5,17 @@ import App from './App'
 describe('App', () => {
   const app = shallow(<App />)
 
+  // test render
   it('renders the `Flashcard Pro` title', () => {
     expect(app.find('h2').text()).toEqual('Flashcard Pro')
+  })
+
+  it('renders the StackList', () => {
+    // console.log(app.debug())
+    expect(app.find('Connect(StackList)').exists()).toBe(true)
+  })
+
+  it('renders a link to create new stacks', () => {
+    expect(app.find('Link h4').text()).toEqual('Create a New Stack')
   })
 })
