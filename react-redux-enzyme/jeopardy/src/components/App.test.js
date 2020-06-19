454d73b75196cd4b4a3886a790d3a3ae63ef5f86
dhantosy <dhantosy@gmail.com>
2020-06-19 14:41:39 +0700

    added mock data

---
+++ react-redux-enzyme/jeopardy/src/components/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { App } from './App'
+import { categories } from '../data/fixtures'
+
+const props = { categories }
+
+describe('App', () => {
+  const app = shallow(<App {...props} />)
+
+  
+})
