d04c6b822a6231f95e2313f7530aca552e6b8193
dhantosy <dhantosy@gmail.com>
2020-06-19 10:00:05 +0700

    added test for redux actions and reducers

---
+++ react-redux-enzyme/flashcardpro/src/reducers/index.test.js
@@ -0,0 +1,25 @@
+import rootReducer from './index'
+import * as actions from '../actions'
+import { stack, stacks } from '../data/fixtures'
+
+describe('root reducers', () => {
+
+  it('returns the initial state', () => {
+    expect(rootReducer({}, {})).toEqual({ stack: {}, stacks: [] })
+  })
+
+  it('sets the main stack', () => {
+    expect(rootReducer({}, { type: actions.SET_STACK, stack }))
+      .toEqual({ stack, stacks: [] })
+  })
+
+  it('loads stacks', () => {
+    expect(rootReducer({}, { type: actions.LOAD_STACKS, stacks }))
+      .toEqual({ stack: {}, stacks })
+  })
+
+  it('adds a stack', () => {
+    expect(rootReducer({}, { type: actions.ADD_STACK, stack }))
+      .toEqual({ stack: {}, stacks: [stack] })
+  })
+})
