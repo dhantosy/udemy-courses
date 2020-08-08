3e64ebe57b14aabab00bdf4b8a53f784ebad3abb
dhantosy <dhantosy@gmail.com>
2020-08-08 16:51:43 +0700

    added dynamic link to detail page

--- modern-ssr-nextjs/tvshow/components/Thumbnail/index.js
+++ modern-ssr-nextjs/tvshow/components/Thumbnail/index.js
@@ -1,12 +1,20 @@
 import styles from './Thumbnail.module.scss'
+import Link from 'next/link'
 
-const Thumbnail = ({ imageUrl = 'https://via.placeholder.com/210x295', caption}) => {
-  return (
-    <div className='thumbnail'>
-      <img src={imageUrl} alt={caption} className={styles.thumbnail__image} />
-      <h4 className={styles.thumbnail__caption}>{caption}</h4>
-    </div>
-  )
-}
+const Thumbnail = ({ 
+  imageUrl = 'https://via.placeholder.com/210x295', 
+  caption,
+  href = '',
+  as = '',
+}) => (
+  <div className='thumbnail'>
+    <Link href={href} as={as}>
+      <a>
+        <img src={imageUrl} alt={caption} className={styles.thumbnail__image} />
+        <h4 className={styles.thumbnail__caption}>{caption}</h4>
+      </a>
+    </Link>
+  </div>
+)
 
 export default Thumbnail
--- modern-ssr-nextjs/tvshow/pages/[country]/index.js
+++ modern-ssr-nextjs/tvshow/pages/[country]/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Thumbnail from '../../components/Thumbnail'
 
-const Home = ({ shows }) => {
+const Home = ({ shows, country }) => {
 
   // fetch api from client side
   // useEffect(() => {
@@ -15,7 +15,12 @@ const Home = ({ shows }) => {
       
       return (
         <li key={index}>
-          <Thumbnail imageUrl={show.image?.medium || undefined} caption={show.name} />
+          <Thumbnail 
+            imageUrl={show.image?.medium || undefined} 
+            caption={show.name} 
+            href='/[country]/[showId]'
+            as={`/${country}/${show.id}`}
+          />
         </li>
       )
     })
@@ -29,17 +34,19 @@ const Home = ({ shows }) => {
 }
 
 Home.getInitialProps = async context => {
-  const country = context.query.country || 'us'
   //getInitialprops
   //can only access props from the params passed in this component
   //not compatible with client side props
+
+  const country = context.query.country || 'us'
 
   const response = await axios.get(
     `http://api.tvmaze.com/schedule?country=${country}&date=2014-12-01`
   )
 
   return {
-    shows: response.data
+    shows: response.data,
+    country
   }
 }
 
