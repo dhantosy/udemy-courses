06b00760147350a6aac3d5cc5ec25b603f64ea1f
dhantosy <dhantosy@gmail.com>
2020-08-08 18:29:23 +0700

    added header component

---
+++ modern-ssr-nextjs/tvshow/components/Header/index.js
@@ -0,0 +1,21 @@
+const Header = () => {
+
+  return (
+    <div className='header'>
+      Header
+
+      <style jsx>
+        {`
+        .header {
+          padding: 20px;
+          background: #333;
+          color: #fff;
+          text-align: center;
+        }
+      `}
+      </style>
+    </div>
+  )
+}
+
+export default Header
