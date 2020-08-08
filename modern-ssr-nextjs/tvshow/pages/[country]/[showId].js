42f2144d7b86362ce6a7ddaa20fd9b8085ae8108
dhantosy <dhantosy@gmail.com>
2020-08-08 17:26:05 +0700

    added fetch data and props

--- modern-ssr-nextjs/tvshow/pages/[country]/[showId].js
+++ modern-ssr-nextjs/tvshow/pages/[country]/[showId].js
@@ -1,9 +1,32 @@
-const ShowDetails = () => {
+import axios from 'axios'
+import parse from 'html-react-parser'
+import Cast from '../../components/Cast'
+
+const ShowDetails = ({ show }) => {
+  const { name, image, summary, _embedded } = show;
   return (
-    <div>
-      show ShowDetails
+    <div className='show-details'>
+      <div className='show-details__poster' style={{ backgroundImage: `url(${image.original})` }}></div>
+      <h1>{name}</h1>
+      <div>{parse(summary)}</div>
+      <Cast cast={_embedded.cast} />
+
+      <style jsx>{`
+        .show-details__poster {
+          height: 200px;
+          background-size: cover
+        }
+      `}</style>
     </div>
   )
+}
+
+ShowDetails.getInitialProps = async () => {
+  const response = await axios.get('http://api.tvmaze.com/shows/1?embed=cast')
+
+  return {
+    show: response.data
+  }
 }
 
 export default ShowDetails
