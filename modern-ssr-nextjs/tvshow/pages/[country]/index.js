d1f5e7cc7f99357c49de831a96f5c351f8d1b994
dhantosy <dhantosy@gmail.com>
2020-08-08 15:22:19 +0700

    added fetching data in the server side

--- modern-ssr-nextjs/tvshow/pages/[country]/index.js
+++ modern-ssr-nextjs/tvshow/pages/[country]/index.js
@@ -1,16 +1,26 @@
 import { useEffect } from 'react'
 import axios from 'axios'
 
-const CountryTest = () => {
+const CountryTest = (props) => {
+  const { shows } = props
 
-  useEffect(() => {
-    axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
-      .then(response => console.log(response.data))
-  }, [])
+  // fetch api from client side
+  // useEffect(() => {
+  //   axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+  //     .then(response => console.log(response.data))
+  // }, [])
 
   return (
     <div>country test</div>
   )
+}
+
+CountryTest.getInitialProps = async () => {
+  const response = await axios.get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+
+  return {
+    shows: response.data
+  }
 }
 
 export default CountryTest
