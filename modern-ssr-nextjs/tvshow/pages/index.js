fba4e22e1e7e1d8e0663c007a0fab29b1aff03fc
dhantosy <dhantosy@gmail.com>
2020-08-09 08:50:13 +0700

    added change url in server side

--- modern-ssr-nextjs/tvshow/pages/index.js
+++ modern-ssr-nextjs/tvshow/pages/index.js
@@ -1,9 +1,27 @@
+import Router from 'next/router'
+
 const Home = () => {
+  // render in client side
+  console.log('browser', process.browser)
+
   return (
     <div>
       This is homepage
     </div>
   )
+}
+
+Home.getInitialProps = context => {
+  // render in server side
+  console.log('browser', process.browser)
+
+  const country = context.query.country || 'us'
+
+  process.browser ?
+    Router.replace('/[country', `${country}`) :
+    context.res.writeHead(302, { Location: `/${country}` })
+
+  context.res.end()
 }
 
 export default Home;
