49b0a82a7494d820a8982bf288a421d9fdb853a2
dhantosy <dhantosy@gmail.com>
2020-08-09 16:36:34 +0700

    added delete song mutation handler

--- graphql-with-react/client/components/SongList.js
+++ graphql-with-react/client/components/SongList.js
@@ -6,10 +6,20 @@ import query from '../queries/fetchSongs'
 
 class SongList extends Component {
 
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } });
+  }
+
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className='collection-item'>{song.title}</li>
+        <li key={id} className='collection-item'>
+          {title}
+          <i
+            className='material-icons'
+            onClick={() => this.onSongDelete(id)}
+          >delete</i>
+        </li>
       )
     })
   }
