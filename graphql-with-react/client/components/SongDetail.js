c602e750dbd4012d2bbb42a580bd6103e4d00586
dhantosy <dhantosy@gmail.com>
2020-08-15 15:07:08 +0700

    integrate react router with graphql

--- graphql-with-react/client/components/SongDetail.js
+++ graphql-with-react/client/components/SongDetail.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
@@ -9,4 +11,6 @@ class SongDetail extends Component {
    )
  }}
 
- export default SongDetail
+ export default graphql(fetchSong, {
+   options: (props) => { return { variables: { id: props.params.id }}}
+ })(SongDetail)
