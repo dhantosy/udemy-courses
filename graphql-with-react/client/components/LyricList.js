e419d6fd201dbbe1470f1723d4129932ed05ee5a
dhantosy <dhantosy@gmail.com>
2020-08-16 11:25:59 +0700

    display likes count

--- graphql-with-react/client/components/LyricList.js
+++ graphql-with-react/client/components/LyricList.js
@@ -9,16 +9,19 @@ class LyricList extends Component {
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className='collection-item'>
           {content}
-          <i 
-            className='material-icons'
-            onClick={(() => this.onLike(id))}
-          >
+          <div className='vote-box'>
+            <i
+              className='material-icons'
+              onClick={(() => this.onLike(id))}
+            >
               thumb_up
-          </i>
+            </i>
+            {likes}
+          </div>
         </li>
       )
     })
