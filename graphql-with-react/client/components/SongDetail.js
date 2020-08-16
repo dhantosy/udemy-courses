f4d2bbecceb79340c2563cb95ef2b4cbf1306a9f
dhantosy <dhantosy@gmail.com>
2020-08-16 09:09:21 +0700

    handle when data is loading

--- graphql-with-react/client/components/SongDetail.js
+++ graphql-with-react/client/components/SongDetail.js
@@ -4,12 +4,17 @@ import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
-   return (
-     <div>
-       <h3>Song Detail</h3>
-     </div>
-   )
- }}
+    const { song } = this.props.data;
+
+    if (!song) { return <div>Loading...</div> }
+
+    return (
+      <div>
+        <h3>{song.title}</h3>
+      </div>
+    )
+  }
+}
 
  export default graphql(fetchSong, {
    options: (props) => { return { variables: { id: props.params.id }}}
